7be83ee1fe63c1994e2285393793a4b1dc9abb1a
Arimatea Neto <arineto30@gmail.com>
2015-10-25 19:28:44 -0300

    Corrigindo o LowerBound

--- src/dfsa/LowerBound.java
+++ src/dfsa/LowerBound.java
@@ -4,10 +4,10 @@ public class LowerBound implements Estimator{
 	
 	public int estimate(int successSlots, int collisionSlots){
 		int response;
-		if( (successSlots == 0) && (collisionSlots == 0)){
+		if(collisionSlots == 0){
 			response = 50;
 		}else{
-			response = successSlots + 2*collisionSlots;
+			response = 2*collisionSlots;
 		}
 		return response;
 	}
